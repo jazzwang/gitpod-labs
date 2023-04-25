aeb1392878593f80b7681b6a4dfd93ed57474b93
jazzwang <jazzwang.tw@gmail.com>
2023-04-25 19:16:15 +0800

    add humana toc API response JSON and sampling toc scala code

---
+++ mrf/humana/sample-toc-by-size.scala
@@ -0,0 +1,7 @@
+val toc = spark.read.json("2023-03-27_humana_toc.json.gz")
+toc.printSchema
+val toc_sorted = toc.select(explode($"aaData").as("toc")).select($"toc.size",$"toc.name").orderBy($"size".desc)
+println(s"Total count = " + toc_sorted.count)
+val samples = toc_sorted.sample(0.001)
+println(s"sample count = " + samples.count)
+samples.write.json("sample-toc")
